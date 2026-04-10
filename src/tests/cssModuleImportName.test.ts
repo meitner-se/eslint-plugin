0e82475996be5d7c706e97b477b707b086363fa1
Mattis Abrahamsson <mattis.abrahamsson@gmail.com>
2026-04-10 10:30:41 +0200

    Update tests and readme

--- src/tests/cssModuleImportName.test.ts
+++ src/tests/cssModuleImportName.test.ts
@@ -13,10 +13,10 @@ const ruleTester = new RuleTester({
 
 ruleTester.run("cssModuleImportName", cssModuleImportName, {
     valid: [
-        // Default option: "classes"
+        // Default option: "classes" with different filenames and extensions
         'import classes from "./styles.module.css";',
-        'import classes from "./styles.module.scss";',
-        'import classes from "./styles.module.less";',
+        'import classes from "./Component.module.scss";',
+        'import classes from "./header.module.less";',
         // Non-CSS-module imports should be ignored
         'import styles from "./styles.css";',
         'import foo from "./foo";',
@@ -29,9 +29,17 @@ ruleTester.run("cssModuleImportName", cssModuleImportName, {
         'import * as styles from "./styles.module.css";',
         // Default + named together should be ignored
         'import classes, { something } from "./styles.module.css";',
-        // Custom option
+        // Custom option with different filenames
         {
-            code: 'import styles from "./styles.module.css";',
+            code: 'import styles from "./Button.module.css";',
+            options: [{ name: "styles" }],
+        },
+        {
+            code: 'import styles from "./layout.module.scss";',
+            options: [{ name: "styles" }],
+        },
+        {
+            code: 'import styles from "./theme.module.less";',
             options: [{ name: "styles" }],
         },
     ],
@@ -47,39 +55,39 @@ ruleTester.run("cssModuleImportName", cssModuleImportName, {
             output: 'import classes from "./styles.module.css";',
         },
         {
-            code: 'import s from "./component.module.scss";',
+            code: 'import s from "./Button.module.scss";',
             errors: [
                 {
                     messageId: "cssModuleImportName" as const,
                     data: { expected: "classes", actual: "s" },
                 },
             ],
-            output: 'import classes from "./component.module.scss";',
+            output: 'import classes from "./Button.module.scss";',
         },
         {
-            code: 'import css from "./component.module.less";',
+            code: 'import css from "./header.module.less";',
             errors: [
                 {
                     messageId: "cssModuleImportName" as const,
                     data: { expected: "classes", actual: "css" },
                 },
             ],
-            output: 'import classes from "./component.module.less";',
+            output: 'import classes from "./header.module.less";',
         },
         // Deeper import path
         {
-            code: 'import styles from "../../shared/styles.module.css";',
+            code: 'import styles from "../../shared/layout.module.css";',
             errors: [
                 {
                     messageId: "cssModuleImportName" as const,
                     data: { expected: "classes", actual: "styles" },
                 },
             ],
-            output: 'import classes from "../../shared/styles.module.css";',
+            output: 'import classes from "../../shared/layout.module.css";',
         },
         // Custom option: should report when not matching custom name
         {
-            code: 'import classes from "./styles.module.css";',
+            code: 'import classes from "./Card.module.css";',
             options: [{ name: "styles" }],
             errors: [
                 {
@@ -87,7 +95,7 @@ ruleTester.run("cssModuleImportName", cssModuleImportName, {
                     data: { expected: "styles", actual: "classes" },
                 },
             ],
-            output: 'import styles from "./styles.module.css";',
+            output: 'import styles from "./Card.module.css";',
         },
     ],
 });
